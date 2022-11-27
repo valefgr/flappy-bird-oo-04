f1c02d8d888af03458dcfae083e04b869636c668
valentina figueroa gutierrez <paletinafgv@MacBook-Air-de-valentina.local>
2022-11-27 09:48:28 -0600

    proyectooo

--- script.js
+++ script.js
@@ -1,63 +1,77 @@
 const juego = {
-    timerId: 0,
-    gravedad: 2,
-  
-    aleatorio: function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    },
-  
-    // 2. Agrega el método verificaColision()
-   
-  
-    loop: function () {
-      bird.efectoGravedad();
-      bird.dibujar(); 
-      // 3. llama a juego.verificaColision();
-      
-    },
-  
-    iniciar: function () {
-      document.addEventListener("keyup", bird.mover);
-      juego.timerId = setInterval(juego.loop, 20);
-    },
-  
-    // 6. Agrega el método terminar()
-  
-    
-    // 4. Agrega el método mostrarGameOver()
-    
+  timerId: 0,
+  gravedad: 2,
 
-    // 5. Agrega el método pararEfectos()
-    
-  };
-  
+  aleatorio: function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+
+  verificaColision: function () {
+    if (bird.colision()) {
+      juego.terminar();
+    }
+  },
+
+  loop: function () {
+    bird.efectoGravedad();
+    bird.dibujar(); 
+    juego.verificaColision();
+  },
+
+  iniciar: function () {
+    document.addEventListener("keyup", bird.mover);
+    juego.timerId = setInterval(juego.loop, 20);
+  },
+
+  terminar: function () {  
+    clearInterval(juego.timerId);
+    juego.mostrarGameOver();
+    juego.pararEfectos();
+  },
+
+  mostrarGameOver: function () {
+    const mensaje = document.querySelector(".message");
+    mensaje.setAttribute("id", "game-over");
+  },
+
+  pararEfectos: function () {
+    let ground = document.querySelector(".ground");
+    ground.removeAttribute("id");
+
+    bird.div.setAttribute("id", "fall");
+  },
+};
+
 
 
 
 const bird = {
-    div: document.querySelector(".bird"),
-    bottom: juego.aleatorio(10, 570),
-    left: 250,
-    width: 60,
-    height: 45,
-  
-    efectoGravedad: function () {
-      bird.bottom -= juego.gravedad;
-    },
-  
-    dibujar: function () {
-      bird.div.style.bottom = bird.bottom + "px";
-      bird.div.style.left = bird.left + "px";
-    },
-  
-    mover: function () {
-      bird.bottom += 40;
+  div: document.querySelector(".bird"),
+  bottom: juego.aleatorio(10, 570),
+  left: 250,
+  width: 60,
+  height: 45,
+
+  efectoGravedad: function () {
+    bird.bottom -= juego.gravedad;
+  },
+
+  dibujar: function () {
+    bird.div.style.bottom = bird.bottom + "px";
+    bird.div.style.left = bird.left + "px";
+  },
+
+  mover: function () {
+    bird.bottom += 40;
+  },
+
+  colision: function () {
+      if (bird.bottom < 0) {
+        return true;
+      }
     },
 
-    // 1. Agrega el método colision()
-  
-  
-   
+ 
 }
 
 juego.iniciar();
